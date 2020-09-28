3f8b58a9d81d16ec36f6f214fad5f9ca471e6ef2
chandresh R <chandreshraj007@gmail.com>
2020-09-28 23:00:01 +0530

    quote completed

---
+++ script.js
@@ -0,0 +1,62 @@
+// page elements variables
+
+const quoteContainer = document.getElementById("qoute-container");
+const quotesText = document.getElementById("quote");
+const authorName = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// loading event
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+// API to get the quotes from ONLINE
+
+async function getQuote() {
+  loading();
+  const apiurl = "https://type.fit/api/quotes";
+  try {
+    const res = await fetch(apiurl);
+    let Quotes = [];
+    Quotes = await res.json();
+    const data = Quotes[Math.floor(Math.random() * Quotes.length)];
+    authorName.innerText = data.author != null ? data.author : "Unkown";
+    if (data.text.length > 120) {
+      quotesText.classList.add("long-quote");
+    } else {
+      quotesText.classList.remove("long-quote");
+    }
+    quotesText.innerText = data.text;
+    complete();
+  } catch (error) {
+    if (error) {
+      getQuote();
+    }
+  }
+}
+
+// to tweet the quote
+function tweetQuote() {
+  const quote = quotesText.innerText;
+  const author = authorName.innerText;
+  const twitterurl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterurl, "_blank");
+}
+
+// loading the Quotes on load
+getQuote();
+
+//listen to twitter button
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
