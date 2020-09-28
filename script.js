178f40b71cc7038af901f162d422f879f7e085c3
chandresh R <chandreshraj007@gmail.com>
2020-09-28 23:17:13 +0530

    conmetic changes and code refactor

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// page elements variables
-
 const quoteContainer = document.getElementById("qoute-container");
 const quotesText = document.getElementById("quote");
 const authorName = document.getElementById("author");
@@ -7,44 +5,41 @@ const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
 
-// loading event
-
-function loading() {
+function showLoadingSpinner() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-function complete() {
+function hideLoadingSpinner() {
   if (!loader.hidden) {
     quoteContainer.hidden = false;
     loader.hidden = true;
   }
 }
 
-// API to get the quotes from ONLINE
-
-async function getQuote() {
-  loading();
+const getQuote = async () => {
+  showLoadingSpinner();
   const apiurl = "https://type.fit/api/quotes";
   try {
     const res = await fetch(apiurl);
     let Quotes = [];
     Quotes = await res.json();
     const data = Quotes[Math.floor(Math.random() * Quotes.length)];
-    authorName.innerText = data.author != null ? data.author : "Unkown";
+    authorName.innerText =
+      data.author != null ? "- " + data.author : " - Unkown";
     if (data.text.length > 120) {
       quotesText.classList.add("long-quote");
     } else {
       quotesText.classList.remove("long-quote");
     }
     quotesText.innerText = data.text;
-    complete();
+    hideLoadingSpinner();
   } catch (error) {
     if (error) {
       getQuote();
     }
   }
-}
+};
 
 // to tweet the quote
 function tweetQuote() {
@@ -54,9 +49,8 @@ function tweetQuote() {
   window.open(twitterurl, "_blank");
 }
 
-// loading the Quotes on load
+// Inital Point
 getQuote();
 
-//listen to twitter button
 newQuoteBtn.addEventListener("click", getQuote);
 twitterBtn.addEventListener("click", tweetQuote);
